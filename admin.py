67c4c9cef268f45c61c29952a5a966e9173c5fa7
Diya Barahate <66784537+Diya0812@users.noreply.github.com>
2026-05-02 11:30:28 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,39 +1,39 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission
 
 
-class ChoiceInline(admin.TabularInline):
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
 
-class QuestionInline(admin.TabularInline):
+class QuestionInline(admin.StackedInline):
     model = Question
-    extra = 1
-
-
-class LessonInline(admin.TabularInline):
-    model = Lesson
-    extra = 1
+    extra = 2
 
 
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ['name']
+    list_display = ['name', 'description']
 
 
 class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
     list_display = ['title', 'course']
 
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ['question_text', 'lesson']
+    list_display = ['question_text', 'lesson', 'grade']
 
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Enrollment)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
