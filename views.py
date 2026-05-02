f399af7d3ecc84b1920dd80c083bac059767784e
Diya Barahate <66784537+Diya0812@users.noreply.github.com>
2026-05-02 11:31:29 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Course, Enrollment, Choice, Submission
+
+
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+
+    enrollment = Enrollment.objects.filter(
+        user=request.user,
+        course=course
+    ).first()
+
+    submission = Submission.objects.create(enrollment=enrollment)
+
+    selected_choice_ids = request.POST.getlist('choice')
+    for choice_id in selected_choice_ids:
+        choice = Choice.objects.get(pk=choice_id)
+        submission.choices.add(choice)
+
+    submission.save()
+
+    return HttpResponseRedirect(
+        reverse('onlinecourse:show_exam_result', args=(course_id, submission.id))
+    )
+
+
 def show_exam_result(request, course_id, submission_id):
     course = get_object_or_404(Course, pk=course_id)
     submission = get_object_or_404(Submission, pk=submission_id)
